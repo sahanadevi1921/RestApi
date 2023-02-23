dd941429be91342268e30360041b6d07263d4b1f
sahanadevi1921 <126045322+sahanadevi1921@users.noreply.github.com>
2023-02-23 10:44:23 +0530

    Add files via upload

---
+++ Day-3/src/main/java/com/example/demo/model/Customer.java
@@ -0,0 +1,42 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+	@Id
+	private int id;
+	private String name;
+	private int age;
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
+
+}
